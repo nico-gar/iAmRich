d200d8d0f20dd4fdf5eff8c11b3a2cca5b38898e
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2023-05-30 08:34:23 -0700

    Initial Commit

---
+++ iAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iAmRich
+//
+//  Created by Nicolas Garaycochea on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
